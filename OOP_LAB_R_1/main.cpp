f0dd743ea98a2e178aeb3beb27ce2e98f4c6f0c8
Ruhshona <r.jumanazarova@newuu.uz>
2025-08-29 15:52:11 +0500

    Solved all problems

--- OOP_LAB_R_1/main.cpp
+++ OOP_LAB_R_1/main.cpp
@@ -1,7 +1,16 @@
 
 #include <iostream>
 using namespace std;
-int main(int argc, const char * argv[]) {
-  cout << "Hello, World!\n";
+int main(){
+    int num;
+    cin>>num;
+    int sum=0;
+    int r;
+    while(num>0){
+        r=num%10;
+        sum+=r;
+        num/=10;
+    }
+    cout<<sum<<endl;
     return 0;
 }
